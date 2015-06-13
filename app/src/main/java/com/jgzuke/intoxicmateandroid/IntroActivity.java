0615cb5e3f08b983b7da49daf50cf866cbf0931c
jgzuke <jgzuke@gmail.com>
2015-06-13 14:50:04 -0400

    Initial

---
+++ app/src/main/java/com/jgzuke/intoxicmateandroid/IntroActivity.java
@@ -0,0 +1,48 @@
+package com.jgzuke.intoxicmateandroid;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+/**
+ * Created by jgzuke on 15-06-13.
+ */
+public class IntroActivity extends AppIntro {
+
+    // Please DO NOT override onCreate. Use init
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        // Add your slide's fragments here
+        // AppIntro will automatically generate the dots indicator and buttons.
+        /*addSlide(new FirstSlide(), getApplicationContext());
+        addSlide(new SecondSlide(), getApplicationContext());
+        addSlide(new ThirdSlide(), getApplicationContext());
+        addSlide(new FourthSlide(), getApplicationContext());
+*/
+        
+        // OPTIONAL METHODS
+        // Override bar/separator color
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip button
+        showSkipButton(false);
+
+        // Turn vibration on and set intensity
+        // NOTE: you will probably need to ask VIBRATE permesssion in Manifest
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed() {
+        // Do something when users tap on Done button.
+    }
+}
