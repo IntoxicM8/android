96021fa87bee8dd5f79a2122c5c729858dc0f214
Sadman Kazi <sadmansazidk@gmail.com>
2015-06-13 19:32:07 -0400

    added uber skeleton

---
+++ app/src/main/java/com/jgzuke/intoxicmateandroid/model/UberModel.java
@@ -0,0 +1,16 @@
+package com.jgzuke.intoxicmateandroid.model;
+
+import com.jgzuke.intoxicmateandroid.Constants;
+
+/**
+ * Helper class that overrides the {@link #toString()} method to print pretty json. All models
+ * extend this class.
+ */
+public class UberModel {
+
+    @Override
+    public String toString() {
+        return Constants.GSON.toJson(this);
+    }
+
+}
