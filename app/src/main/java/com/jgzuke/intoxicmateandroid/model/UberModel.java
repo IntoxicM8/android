248886c90a7b4930e5653a486c8c2f84a5b5dd7f
Sadman Kazi <sadmansazidk@gmail.com>
2015-06-13 20:33:59 -0400

    pls dont break

---
+++ app/src/main/java/com/jgzuke/intoxicmateandroid/model/UberModel.java
@@ -0,0 +1,16 @@
+package com.jgzuke.intoxicmateandroid.model;
+
+import com.rahul.uberapi.android.demo.Constants;
+
+/**
+ * Helper class that overrides the {@link #toString()} method to print pretty json. All models
+ * extend this class.
+ */
+public class UberModel {
+
+    @Override
+    public String toString() {
+        return Constants.GSON.toJson(this);
+    }
+
+}
