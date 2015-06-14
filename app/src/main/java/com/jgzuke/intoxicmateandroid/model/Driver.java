cc749f73aaa8fcacbc0a5d57b1c7674875bbf2b1
Sadman Kazi <sadmansazidk@gmail.com>
2015-06-14 03:24:18 -0400

    Added Driver model

---
+++ app/src/main/java/com/jgzuke/intoxicmateandroid/model/Driver.java
@@ -0,0 +1,15 @@
+package com.jgzuke.intoxicmateandroid.model;
+
+public Driver extends UberModel {
+    String phone_number;
+    public String getPhoneNumber() { return phone_number; }
+    
+    int rating;
+    public int getRating() { return rating; }
+    
+    String picture_url;
+    public String getPicture() { return picture_url; }
+    
+    String name;
+    public String getName() { return name; }
+}
