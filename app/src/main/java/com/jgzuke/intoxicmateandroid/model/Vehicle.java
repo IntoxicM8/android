ad370d7a4df7fe86c339b9fabb1777fb4014797f
Sadman Kazi <sadmansazidk@gmail.com>
2015-06-14 03:27:31 -0400

    Added vehicle model

---
+++ app/src/main/java/com/jgzuke/intoxicmateandroid/model/Vehicle.java
@@ -0,0 +1,15 @@
+package com.jgzuke.intoxicmateandroid.model;
+
+public Vehicle extends UberModel {
+	String make;
+	public String getMake() { return make; }
+	
+	String model;
+	public String getModel() { return model; }
+	
+	String license_plate;
+	public String getLicense() { return license_plate; }
+	
+	String picture_url;
+	public String getPicture() { return picture_url; }
+}
